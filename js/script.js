0ba3dbdb7b6147e2a65031fe612b3fc990df10d0
Amory <108595086+amorychambers@users.noreply.github.com>
2022-08-19 22:14:45 +0100

    Added change event for number of profiles

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector(".num-users");
 
-const getData = async function (){
-    const usersRequest = await fetch('https://randomuser.me/api?results=5');
+const getData = async function (numUsers){
+    const usersRequest = await fetch(`https://randomuser.me/api?results=${numUsers}`);
     const data = await usersRequest.json();
     const userResults = data.results;
     console.log(userResults);
     displayUsers(userResults);
-
 };
 
-getData();
+getData(1);
 
 function displayUsers (userResults){
     randomFolks.innerHTML = "";
@@ -20,8 +20,11 @@ function displayUsers (userResults){
         let userDiv = document.createElement("div");
         userDiv.innerHTML = `<h3>${name}</h3><p>${country}</p><img src=${imageURL} alt="User avatar">`;
         randomFolks.append(userDiv);
-        
     }
-
 };
+
+selectUserNumber.addEventListener("change", function(e){
+    const numUsers = e.target.value;
+    getData(numUsers);
+});
 
